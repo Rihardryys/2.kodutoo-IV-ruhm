1ea83653b16dc9d21117e60f07cf99f98d2ecc1d
Rihardryys <Rihardryys@users.noreply.github.com>
2017-01-09 19:37:59 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,15 +1,14 @@
 <?php
-
-require("config.php");
+require("../config.php");
 session_start();
 	
 	
 	
-	$database = "php1"; 
+	$database = "if16_rihard_4"; 
 	function signup($email, $password, $signupEesnimi, $signupPerenimi){
 		
 		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
-		$stmt = $mysqli->prepare("INSERT INTO user_table (email, enimi, pnimi, password) VALUES (?, ?, ?, ?)");
+		$stmt = $mysqli->prepare("INSERT INTO user_sample (email, enimi, pnimi, password) VALUES (?, ?, ?, ?)");
 		echo $mysqli->error;
 		//asendan küsimärgid
 		//iga märgi kohta tuleb lisada üks täht - mis muutuja on
@@ -34,7 +33,7 @@ session_start();
 		
 		$stmt = $mysqli->prepare("
 			SELECT id, email, password
-			FROM user_table
+			FROM user_sample
 			WHERE email = ?
 		");
 		
@@ -86,12 +85,76 @@ session_start();
 		
 	}
 	
+	function saveEvent($age, $color) {
+		
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+		
+		$stmt = $mysqli->prepare("INSERT INTO whistle (age, color) VALUE (?, ?)");
+		echo $mysqli->error;
+		
+		$stmt->bind_param("is", $age, $color);
+		
+		if ( $stmt->execute() ) {
+			echo "õnnestus";
+		} else {
+			echo "ERROR ".$stmt->error;
+		}
+		
+	}
 	
-	
-	
-	
-	
-	
-	
+	function getAllPeople() {
+		
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
 
+		$stmt = $mysqli->prepare("
+			SELECT id, age, color
+			FROM whistle
+		");
+		$stmt->bind_result($id, $age, $color);
+		$stmt->execute();
+		
+		$results = array();
+		
+		// tsükli sisu tehakse nii mitu korda, mitu rida
+		// SQL lausega tuleb
+		while ($stmt->fetch()) {
+			
+			$human = new StdClass();
+			$human->id = $id;
+			$human->age = $age;
+			$human->lightColor = $color;
+			
+			
+			//echo $color."<br>";
+			array_push($results, $human);
+			
+		}
+		
+		return $results;
+		
+	}
+	
+	
+	function cleanInput($input) {
+		
+		
+		$input = trim($input);
+		
+		
+		// võtab välja \
+		$input = stripslashes($input);
+		
+		// html asendab, nt "<" saab "&lt;"
+		$input = htmlspecialchars($input);
+		
+		return $input;
+		
+	}
+	
+	
+	
+	
+	
+	
+	
 ?>
